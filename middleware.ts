7f41a4b75ba05bdbe2f7636eae5f50a813b06de4
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-02-06 16:28:49 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs"
-import { clerkConfig } from "./app/clerk-config"
 
 export default authMiddleware({
-    publicRoutes: clerkConfig.publicRoutes,
+  publicRoutes: ["/", "/api/user/get-current"],
+  ignoredRoutes: ["/((?!api|trpc)(_next|.+\\.[\\w]+$))"],
 })
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
 
