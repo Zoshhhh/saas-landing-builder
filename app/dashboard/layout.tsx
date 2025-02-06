b9b2964f90fe3608a112c7978a44a2ac62720cc1
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-02-06 16:21:55 +0100

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,34 +11,29 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  try {
-    const { getToken } = auth();
-    const token = await getToken();
-    const clerkUser = await currentUser();
+  const { getToken } = auth();
+  const token = await getToken();
+  const clerkUser = await currentUser();
 
-    if (!token || !clerkUser) {
-      redirect("/sign-in");
-    }
-
-    const user = await getCurrentUser(token);
-
-    if (!user?.isActive) {
-      redirect(STRIPE_PAYMENT_URL);
-    }
-
-    return (
-      <UserWrapper initialData={user}>
-        <SidebarProvider>
-          <div className="flex w-full">
-            <main className="flex-grow w-[calc(100%-240px)]">
-              {children}
-            </main>
-          </div>
-        </SidebarProvider>
-      </UserWrapper>
-    );
-  } catch (error) {
-    console.error("Erreur dans le layout du dashboard:", error);
+  if (!token || !clerkUser) {
     redirect("/sign-in");
   }
+
+  const user = await getCurrentUser(token);
+
+  if (!user?.isActive) {
+    redirect(STRIPE_PAYMENT_URL);
+  }
+
+  return (
+    <UserWrapper initialData={user}>
+      <SidebarProvider>
+        <div className="flex w-full">
+          <main className="flex-grow w-[calc(100%-240px)]">
+            {children}
+          </main>
+        </div>
+      </SidebarProvider>
+    </UserWrapper>
+  );
 }
