8c13814a8accce418443ed391cf9339fe72ff64d
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-02-06 16:35:57 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
+import { clerkConfig } from "./app/clerk-config"
 
-const publicRoutes = ["/", "/sign-in", "/sign-up", "/pricing", "/features", "/api/user/get-current"]
-const isProtectedRoute = (path: string) => !publicRoutes.some((route) => path.startsWith(route))
-
-export default authMiddleware(async ({ auth, request }) => {
-  const path = request.nextUrl.pathname
-
-  if (!auth.userId && isProtectedRoute(path)) {
-    console.log(`Access denied for route: ${path}`)
-    const signInUrl = new URL("/sign-in", request.url)
-    signInUrl.searchParams.set("redirect_url", request.url)
-    return NextResponse.redirect(signInUrl)
-  }
-
-  // Si l'utilisateur est authentifié et essaie d'accéder à /sign-in ou /sign-up, redirigez-le vers /dashboard
-  if (auth.userId && (path === "/sign-in" || path === "/sign-up")) {
-    return NextResponse.redirect(new URL("/dashboard", request.url))
-  }
-
-  return NextResponse.next()
+export default authMiddleware({
+    publicRoutes: clerkConfig.publicRoutes,
 })
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
 
