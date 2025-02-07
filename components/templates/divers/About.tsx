9d225820af6f492c1c433a9a7fafa4fc80747e59
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-02-07 15:27:03 +0100

    Update About.tsx

--- components/templates/divers/About.tsx
+++ components/templates/divers/About.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
-export default function Newsletter() {
+export default function About() {
     return (
         <section className="py-12 bg-gray-100">
             <div className="container mx-auto px-4">
