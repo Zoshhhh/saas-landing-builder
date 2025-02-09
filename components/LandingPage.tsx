9dcf80fb8597a5ffb293452e2b7ab5311e8702fc
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2025-02-09 21:28:26 +0100

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -3,7 +3,7 @@ import { Hero } from "./landing/Hero"
 import { Features } from "./landing/Features"
 import { CTA } from "./landing/CTA"
 import { Footer } from "./landing/Footer"
-import Pricing from "@/components/landing/Pricing";
+import Pricing from "@/components/landing/Pricing"; 
 
 export function LandingPage() {
     return (
